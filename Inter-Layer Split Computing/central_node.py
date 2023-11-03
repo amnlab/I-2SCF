45031922678853082ada90722a9432b843b74efc
AMN LAB <143478273+amnlab@users.noreply.github.com>
2023-11-03 11:53:31 +0900

    Update central_node.py

--- Inter-Layer Split Computing/central_node.py
+++ Inter-Layer Split Computing/central_node.py
@@ -5,14 +5,6 @@ print("=========================================================")
 print("=================Central node execute====================")
 print("=========================================================")
 
-#print("datarate 0<->1 : ", inter_intra.information[0][1], "Mbps, datarate 1<->2 : ", inter_intra.information[1][2], "Mbps")
-
-#print("node : ", inter_intra.information[0][0], "   computing power : ", inter_intra.information[0][3])
-#print("node : ", inter_intra.information[1][0], "   computing power : ", inter_intra.information[1][3])
-#print("node : ", inter_intra.information[2][0], "   computing power : ", inter_intra.information[2][3])
-#F_l = [40, 19, 38, 19, 38, 38, 19, 38, 38, 9, 9, 9, 0] #GMac
-#F_l = [81, 38, 76, 38, 76, 76, 38, 76, 76, 18, 18, 18, 0] #GFlop
-
 T = 3
 R_tt = [inter_intra.information[0][1], inter_intra.information[1][2]]
 C_t = [inter_intra.information[0][5], inter_intra.information[1][5], inter_intra.information[2][5]]
@@ -64,23 +56,13 @@ G = nx.DiGraph(A)
 P = nx.shortest_path(G, source=0, target=last_vertex - 1, weight='weight')
 d = nx.shortest_path_length(G, source=0, target=last_vertex - 1, weight='weight')
 
-
-# pos = nx.nx_pydot.graphviz_layout(G, prog='dot')
-# nx.draw(G, pos, with_labels=True, font_weight='bold')
-# plt.show()
-
-
-
-#print("Shortest path: ", P)
-#print("Shortest distance: ", d)
-
 Layer_ = []
 for i in range(0, len(P) - 1):
     if((P[i] + 1)%3 == 0):
         Layer_.append((P[i] + 1) % 3 + 3)
     else:
         Layer_.append((P[i] + 1)%3)
-#print(Layer_)
+
 Tier_ = []
 Tier_print = []
 if  Layer_.count(1) != 0:
