fba78078dfdf514f637e9e2d675cad9d0c6f4372
AMN LAB <143478273+amnlab@users.noreply.github.com>
2023-11-27 16:38:17 +0900

    Update padding_add.py

--- Inter- and Intra-Layer Split Computing/padding_add.py
+++ Inter- and Intra-Layer Split Computing/padding_add.py
@@ -2,13 +2,30 @@ import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
 
-def add_padding(input_data, split_point, index):
-    if index == 0:
-        padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
-        padded_data = padded_data_zero[:, 0:split_point + 2, :, :]
-        return padded_data
+def add_padding(input_data, split_point1, split_point2, index):
+    if split_point1 == 0:
+        if index == 1:
+            padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
+            padded_data = padded_data_zero[:, 0:split_point2 + 2, :, :]
+            return padded_data
 
-    elif index == 1:
-        padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
-        padded_data = padded_data_zero[:, split_point:, :, :]
-        return padded_data
+        elif index == 2:
+            padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
+            padded_data = padded_data_zero[:, split_point2:, :, :]
+            return padded_data
+
+    else:
+        if index == 0:
+            padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
+            padded_data = padded_data_zero[:, 0:split_point1 + 2, :, :]
+            return padded_data
+
+        elif index == 1:
+            padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
+            padded_data = padded_data_zero[:, split_point1:split_point2 + 2, :, :]
+            return padded_data
+
+        elif index == 2:
+            padded_data_zero = np.pad(input_data, ((0, 0), (1, 1), (1, 1), (0, 0)), mode='constant')
+            padded_data = padded_data_zero[:, split_point2:, :, :]
+            return padded_data
