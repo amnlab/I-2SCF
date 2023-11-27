2f0317dc32eabffaed6eb101383de1ca76b454e2
AMN LAB <143478273+amnlab@users.noreply.github.com>
2023-11-27 16:34:27 +0900

    Update exe.py

--- Inter- and Intra-Layer Split Computing/exe.py
+++ Inter- and Intra-Layer Split Computing/exe.py
@@ -1,1 +1,12 @@
-import UE
+import inter_intra_UE
+import inter_UE
+
+inter = inter_UE.latency
+intra = inter_intra_UE.latency
+print("---------------------------------------------------------------------")
+print("## Compare Infernce Latency")
+print("Inter-Layer Split Computing:", inter)
+print("Inter- and Intra-Layer Split Computing:", intra)
+improve = (inter - intra)/inter * 100
+print("---------------------------------------------------------------------")
+print("Improved", improve,"% than Inter-Layer Split Computing")
