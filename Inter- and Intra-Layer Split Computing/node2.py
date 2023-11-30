a616548b9a4a24fd16273fc0f94edd5c93b378cf
AMN LAB <143478273+amnlab@users.noreply.github.com>
2023-11-30 13:05:02 +0900

    Update node2.py

--- Inter- and Intra-Layer Split Computing/node2.py
+++ Inter- and Intra-Layer Split Computing/node2.py
@@ -17,7 +17,7 @@ from silence_tensorflow import silence_tensorflow
 silence_tensorflow()
 if(0 not in central_node.Tier_print[2]):
     print("======================================================================================")
-    print("Inter Node2 Execute")
+    print("Inter Node 3 Execute")
     print("======================================================================================")
     start_Layer2 = central_node.Tier_print[2][0]
     end_Layer2 = central_node.Tier_print[2][1]
@@ -47,14 +47,14 @@ if(0 not in central_node.Tier_print[2]):
     for i in range (start_Layer2, end_Layer2 + 1):
         compute2 = compute2 + central_node.F_l[i-1]
     latency2 = round(compute2 / inter_intra.information[2][5])
-    print("computing latency2 :", latency2)
+    print("computing latency :", latency2)
     # transmission latency
     Mbyte = sys.getsizeof(intermediate_result2)/1000000
     Mbit = Mbyte * 8
     latency2 = latency2 + round(Mbit / inter_intra.information[1][4]) + round(Mbit / inter_intra.information[1][3])
     tansmission_latency2 = round(Mbit / inter_intra.information[1][4]) + round(Mbit / inter_intra.information[1][3])
-    print('transmission latency2 :', tansmission_latency2)
-    print('inference latency2 :', latency2)
+    print('transmission latency :', tansmission_latency2)
+    print('inference latency :', latency2)
 
     if end_Layer2 == 13:
        eval2 = evaluation.evaluation_for_SC(intermediate_result2, node0.y_test)
